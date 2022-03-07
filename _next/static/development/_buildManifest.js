d4ef87debb95cef4a65e35aec91b89b0911eb818
Nathan Lilly <nathan_lilly@comcast.com>
2022-03-06 22:44:55 -0500

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/guides/drawing-svgs":["static\u002Fchunks\u002Fpages\u002Fguides\u002Fdrawing-svgs.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fguides\u002Fdrawing-svgs"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

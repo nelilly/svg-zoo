bfb7775220f779ca9a3b114b15e4a0033c6ff4fe
nelilly <nathan@lilly.net>
2023-05-29 00:06:50 -0400

    Updates

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/guides/understanding-svgs":["static\u002Fchunks\u002Fpages\u002Fguides\u002Funderstanding-svgs.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fguides\u002Funderstanding-svgs"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
